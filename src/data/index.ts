4d1938959d06f63cddda66db20d8ce515ce5ea14
Paul Pichon F <paul10_barca@hotmail.com>
2024-03-12 17:06:21 -0600

    Create: creacion del SCHEMA del documento USER

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,2 +1,4 @@
 // Exportar de mongo-database
 export * from './mongo/mongo-database';
+// Exportar todo de user.model
+export * from './mongo/models/user.model';
---
+++ src/data/mongo/models/user.model.ts
@@ -0,0 +1,31 @@
+// Mongoose
+import mongoose from "mongoose";
+
+// Schema del usuario
+const userSchema = new mongoose.Schema({
+
+    name: {
+        type: String,
+        required: [ true, 'Name is required']
+    },
+    email: {
+        type: String,
+        required: [ true, 'Email is required'],
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [ true, 'Password is required']
+    },
+    img: {
+        type: String
+    },
+    role: {
+        // se coloca entre [] porque podria tener mas de 1 rol de usuario
+        type: [String],
+        default: ['USER_ROLE'],
+        enum: ['ADMIN_ROLE', 'USER_ROLE']
+    }
+});
+// exportar el schema
+export const UserModel = mongoose.model('User', userSchema);
