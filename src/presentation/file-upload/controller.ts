622cbb36b5d7d24196e5884f86e072335d54c486
Paul Pichon F <paul10_barca@hotmail.com>
2024-04-09 16:40:58 -0600

    Update: colocar archivos en sub-directorios

--- src/presentation/file-upload/controller.ts
+++ src/presentation/file-upload/controller.ts
@@ -29,17 +29,27 @@ export class FileUploadController {
     // subir archivo
     uploadFile = ( req: Request, res: Response) => {
 
-        const files = req.files;
+        // router.post('/single/:type', controller.uploadFile ); --->
+        // el type viene de la ruta de arriba /single/type
+        const type = req.params.type;
+        // tipos validos, esto se puede configurar
+        const validTypes = ['users', 'products', 'categories'];
+        // si no incluye alguno de estos tipos validTypes
+        if( !validTypes.includes( type )) {
+            // mostramos una alerta
+            return res.status( 400 ).json({ error: `Invalid type: ${ type }, valid ones ${ validTypes }`})
+        }
+
         // validar si hay archivos seleccionados
         if ( !req.files || Object.keys( req.files ).length === 0  ) {
             // mostrar un error
             return res.status( 400 ).json({ error: 'No files were selected'});
         }
 
-        // 
+        // renombramo req.files.files
         const file = req.files.file as UploadedFile;
-
-        this.fileUploadService.uploadSingle( file )
+        // 
+        this.fileUploadService.uploadSingle( file, `uploads/${ type }` )
             .then( uploaded => res.json( uploaded ))
             .catch( error => this.handleError( error, res ))
     }
